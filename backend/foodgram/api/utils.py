888a14689796ce9313012c3ec2f5a7c79c0841e3
Юлия Васильева <julka3561@gmail.com>
2022-10-06 13:41:57 +0300

    Deploy!

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import serializers
+
+from recipes.models import Favorite, ShoppingCart
+from users.models import Subscription
 
 
 def get_cart_txt(ingredients):
@@ -15,3 +20,42 @@ def get_cart_txt(ingredients):
     file['Content-Disposition'] = 'attachment; filename={0}'.format(
         filename)
     return file
+
+
+MODELS = {
+    Subscription: {
+        'name': 'author',
+        'err_exist': 'Вы уже подписаны на этого пользователя!',
+        'err_not_exist': 'Вы не подписаны на этого пользователя!',
+    },
+    Favorite: {
+        'name': 'recipe',
+        'err_exist': 'Этот рецепт уже в избранном!',
+        'err_not_exist': 'Этого рецепта нет в избранном!',
+    },
+    ShoppingCart: {
+        'name': 'recipe',
+        'err_exist': 'Этот рецепт уже в корзине!',
+        'err_not_exist': 'Этого рецепта нет в корзине!',
+    },
+}
+
+
+def post_for_actions(user, obj, model):
+    """Function for post request actions."""
+    args = {MODELS[model]['name']: obj,
+            'user': user}
+    post_obj = model(**args)
+    if model.objects.filter(**args).exists():
+        raise serializers.ValidationError(MODELS[model]['err_exist'])
+    post_obj.save()
+
+
+def delete_for_actions(user, obj, model):
+    """Function for delete request actions."""
+    args = {MODELS[model]['name']: obj,
+            'user': user}
+    if not model.objects.filter(**args).exists():
+        raise serializers.ValidationError(MODELS[model]['err_not_exist'])
+    del_obj = get_object_or_404(model, **args)
+    del_obj.delete()
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -11,46 +11,10 @@ from api.permissions import IsAuthorOrAdminOrReadOnly
 from api.serializers import (IngredientSerializer, MiniRecipeSerializer,
                              RecipeCreateSerializer, RecipeSerializer,
                              SubscriptionSerializer, TagSerializer)
-from api.utils import get_cart_txt
+from api.utils import delete_for_actions, get_cart_txt, post_for_actions
 from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Tag)
 from users.models import Subscription, User
-
-MODELS = {
-    Subscription: {
-        'name': 'author',
-        'err_exist': 'Вы уже подписаны на этого пользователя!',
-        'err_not_exist': 'Вы не подписаны на этого пользователя!',
-    },
-    Favorite: {
-        'name': 'recipe',
-        'err_exist': 'Этот рецепт уже в избранном!',
-        'err_not_exist': 'Этого рецепта нет в избранном!',
-    },
-    ShoppingCart: {
-        'name': 'recipe',
-        'err_exist': 'Этот рецепт уже в корзине!',
-        'err_not_exist': 'Этого рецепта нет в корзине!',
-    },
-}
-
-
-def post_for_actions(user, obj, model):
-    args = {MODELS[model]['name']: obj,
-            'user': user}
-    post_obj = model(**args)
-    if model.objects.filter(**args).exists():
-        raise serializers.ValidationError(MODELS[model]['err_exist'])
-    post_obj.save()
-
-
-def delete_for_actions(user, obj, model):
-    args = {MODELS[model]['name']: obj,
-            'user': user}
-    if not model.objects.filter(**args).exists():
-        raise serializers.ValidationError(MODELS[model]['err_not_exist'])
-    del_obj = get_object_or_404(model, **args)
-    del_obj.delete()
 
 
 class SubscriptionsViewSet(viewsets.GenericViewSet):
